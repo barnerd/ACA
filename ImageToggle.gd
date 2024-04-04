b6684e93719e6b27301dcf82a10ad880dea78cbd
barnerd <dibarner@gmail.com>
2024-04-04 12:53:52 -0700

    Layer Toggle
    Added layer toggles for towns & mines

--- ImageToggle.gd
+++ ImageToggle.gd
@@ -1,9 +1,6 @@
 extends CheckButton
 
-@onready var map_layer = $"../../ScrollContainer/Control/HBoxContainer/PanelContainer/MapImages"
+@onready var map_layer = $"../../../ScrollContainer/Control/HBoxContainer/PanelContainer/MapImages"
 
-func _on_toggled(toggled_on: bool) -> void:
-	if toggled_on:
-		map_layer.show()
-	else:
-		map_layer.hide()
+func _on_toggled(toggled_on: bool, _layer: int) -> void:
+	map_layer.set_layer_enabled(_layer, toggled_on)
