12bcd2d0fb1d1981ab0b0d43cbfcb0a13103fd2f
barnerd <dibarner@gmail.com>
2024-04-07 08:23:57 -0700

    Update TerrainColors.gd
    Added some logging

--- TerrainColors.gd
+++ TerrainColors.gd
@@ -1,17 +1,17 @@
 class_name TerrainColors extends Sprite2D
 
 var tile_size: int = 24
-@onready var display_sprite: Sprite2D = $"."
 var display_image: Image
 @export var border_color: Color = Color.BLACK
 
 func _ready() -> void:
-	display_image = display_sprite.texture.get_image()
+	display_image = self.texture.get_image()
 	
 	display_image.resize(8400, 8400, Image.INTERPOLATE_NEAREST)
 	display_image.fill_rect(Rect2i(0, 0, 8400, 8400), border_color)
 	
 	SignalBus.connect_to_signal("savefile_loaded", apply_image)
+	SignalBus.connect_to_signal("tilemap_location_clicked", print_log_image_details)
 
 
 func paint_tile(_position: Vector2i, _color: Color) -> void:
@@ -23,4 +23,13 @@ func paint_tile(_position: Vector2i, _color: Color) -> void:
 
 func apply_image():
 	var new_texture = ImageTexture.create_from_image(display_image)
-	display_sprite.texture = new_texture
+	self.texture = new_texture
+
+
+func print_log_image_details(_coords, _mouse):
+	print(display_image)
+	print(display_image.get_size())
+	print(display_image.get_format())
+	print(self.texture.resource_name)
+	print(self.texture.resource_path)
+
