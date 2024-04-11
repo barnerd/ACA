5dad5e9e80a98b4793956f7696aa3312a435e96b
barnerd <dibarner@gmail.com>
2024-04-10 17:10:50 -0700

    Savefile update
    Additional views and better logging

--- Map View Parser.gd
+++ Map View Parser.gd
@@ -36,7 +36,7 @@ func _on_parse_button_pressed() -> void:
 		parse_map_table(map_table_data)
 	
 	status_textbox.text = "Done"
-	print("Pending Changes count: " + str(pending_changes.size()))
+	print("Tiles updates: " + str(pending_changes.size()))
 
 
 func _on_cancel_button_pressed() -> void:
