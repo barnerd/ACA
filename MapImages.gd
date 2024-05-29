ab979af8cf3173e30db154993c615ae4f3f90d62
barnerd <dibarner@gmail.com>
2024-05-29 14:03:30 -0700

    Optimize TileMap
    Moved TileMap data to TileMap to make it viewable in editor. Removed it from savefile load, so it doesn't take time. Added script to update TileMap offline, from savefile. Added boat layer. Moved TileMap to it's own scene. Connected TileMap to tile_updated signal and update view on update.

--- MapImages.gd
+++ MapImages.gd
@@ -1,8 +1,14 @@
 extends TileMap
 
+enum TileMap_Layers {MAP_IMAGE = 0, MINES = 1, TOWNS = 2}
+enum TileMap_Sources {NONE = -1, SPRITE_SHEET = 0, TOWN = 2, COPPER = 3, IRON = 4, TIN = 5, TITANIUM = 6}
+var mine_type_to_tilemap_source: Array[int] = [TileMap_Sources.TIN, TileMap_Sources.COPPER, TileMap_Sources.IRON, TileMap_Sources.TITANIUM]
+var mine_string_to_mine_type: Array[String] = ["tin", "copper", "iron", "titanium"]
+
 
 func _ready() -> void:
 	SignalBus.connect_to_signal("town_view_settings_changed", on_town_view_settings_changed)
+	SignalBus.connect_to_signal("tile_updated", on_tile_updated)
 
 
 func on_town_view_settings_changed(_settings) -> void:
@@ -42,3 +48,20 @@ func on_town_view_settings_changed(_settings) -> void:
 			set_cell(AgoniaData.MapData.TileMap_Layers.TOWNS, Vector2i(town.location.x, town.location.y), AgoniaData.MapData.TileMap_Sources.TOWN, Vector2i.ZERO)
 		else:
 			set_cell(AgoniaData.MapData.TileMap_Layers.TOWNS, Vector2i(town.location.x, town.location.y), AgoniaData.MapData.TileMap_Sources.NONE)
+
+
+func on_tile_updated(_location: Vector3i) -> void:
+	var tile = AgoniaData.MapData.map_tiles[_location]
+	var loc: Vector2i = Vector2i(tile["x"], tile["y"])
+	
+	var _map_id: int = int(tile["map"])
+	self.set_cell(TileMap_Layers.MAP_IMAGE, loc, TileMap_Sources.SPRITE_SHEET, Vector2i(_map_id % 76, floori(_map_id/76.0)))
+	
+	for mine in tile.mines:
+		var type: int = mine.type
+		self.set_cell(TileMap_Layers.MINES, loc, mine_type_to_tilemap_source[type], Vector2i.ZERO)
+	
+	if AgoniaData.MapData.towns_by_location.has(_location):
+		self.set_cell(TileMap_Layers.TOWNS, loc, TileMap_Sources.TOWN, Vector2i.ZERO)
+	else:
+		self.set_cell(TileMap_Layers.TOWNS, loc, TileMap_Sources.NONE)
---
+++ Tools/update_tilemap_from_file.gd
@@ -0,0 +1,63 @@
+@tool
+extends EditorScript
+
+var map_data
+var tile_map: TileMap
+
+
+# Called when the script is executed (using File -> Run in Script Editor).
+func _run() -> void:
+	print("update tilemap from file")
+	
+	_load_save_file()
+	#print(map_data["groups"])
+	
+	tile_map = get_scene()
+	
+	for tile in map_data["tiles"]:
+		var loc: Vector2i = Vector2i(tile["x"], tile["y"])
+		var _map_id: int = int(tile["map"])
+		tile_map.set_cell(tile_map.TileMap_Layers.MAP_IMAGE, loc, tile_map.TileMap_Sources.SPRITE_SHEET, Vector2i(_map_id % 76, floori(_map_id/76.0)))
+	
+	for mine in map_data["mines_p"]:
+		var loc: Vector2i = Vector2i(mine["x"], mine["y"])
+		var type: int = int(mine["t"])
+		tile_map.set_cell(tile_map.TileMap_Layers.MINES, loc, tile_map.mine_type_to_tilemap_source[type], Vector2i.ZERO)
+	
+	for mine in map_data["mines_t"]:
+		var loc: Vector2i = Vector2i(mine["x"], mine["y"])
+		var type: int = int(mine["t"])
+		tile_map.set_cell(tile_map.TileMap_Layers.MINES, loc, tile_map.mine_type_to_tilemap_source[type], Vector2i.ZERO)
+	
+	for town in map_data["towns"]:
+		var loc: Vector2i = Vector2i(town["x"], town["y"])
+		tile_map.set_cell(tile_map.TileMap_Layers.TOWNS, loc, tile_map.TileMap_Sources.TOWN, Vector2i.ZERO)
+	
+	print("done updating tilemap")
+
+
+func _load_save_file() -> void:
+	print("loading file...")
+	if not FileAccess.file_exists("res://savegame.txt"):
+		return # Error! We don't have a save to load.
+
+	# Load the file line by line and process that dictionary to restore
+	# the object it represents.
+	var save_game_file = FileAccess.open("res://savegame.txt", FileAccess.READ)
+	while save_game_file.get_position() < save_game_file.get_length():
+		var json_string = save_game_file.get_line()
+
+		# Creates the helper class to interact with JSON
+		var json = JSON.new()
+
+		# Check if there is any error while parsing the JSON string, skip in case of failure
+		var parse_result = json.parse(json_string)
+		if not parse_result == OK:
+			print("JSON Parse Error: ", json.get_error_message(), " in ", json_string, " at line ", json.get_error_line())
+			continue
+
+		# Get the data from the JSON object
+		var node_data = json.get_data()
+		
+		if node_data["node_path"] == "/root/AgoniaData/MapDataBase":
+			map_data = node_data
