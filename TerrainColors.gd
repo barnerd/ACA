a41485ef36bc3fa09a4dacd4261580b42b8787b9
barnerd <dibarner@gmail.com>
2024-05-29 14:01:10 -0700

    Terrain_Colors flat file
    Moved Terrain_colors to flat file

--- TerrainColors.gd
+++ TerrainColors.gd
@@ -1,17 +1,47 @@
 class_name TerrainColors extends Sprite2D
 
+# TODO: make images 80 x 80 tiles at 24x24
 var display_image: Image
-@export var border_color: Color = Color.BLACK
+
+
+func _init() -> void:
+	display_image = self.texture.get_image()
+
 
 func _ready() -> void:
+	SignalBus.connect_to_signal("tile_updated", on_tile_updated)
+	SignalBus.connect_to_signal("update_completed", on_update_completed)
+	SignalBus.connect_to_signal("save_game_requested", on_save_game_requested)
+
+
+func reset_image() -> void:
 	display_image = self.texture.get_image()
 	
 	var image_size = Vector2i(AgoniaData.MapData.MAP_SIZE.x * AgoniaData.MapData.TILE_SIZE.x, AgoniaData.MapData.MAP_SIZE.y * AgoniaData.MapData.TILE_SIZE.y)
 	display_image.resize(image_size.x, image_size.y, Image.INTERPOLATE_NEAREST)
-	display_image.fill_rect(Rect2i(0, 0, image_size.x, image_size.y), border_color)
+	display_image.fill_rect(Rect2i(0, 0, image_size.x, image_size.y), Color.BLACK)
 	
-	SignalBus.connect_to_signal("savefile_loaded", apply_image)
-	SignalBus.connect_to_signal("tilemap_location_clicked", print_log_image_details)
+	for loc in AgoniaData.MapData.map_tiles:
+		on_tile_updated(loc)
+	
+	on_update_completed()
+
+
+func on_tile_updated(_location: Vector3i) -> void:
+	var terrain_id: int = AgoniaData.MapData.map_tiles[_location].terrain_id
+	var color: Color = Color.BLACK
+	if terrain_id != -1:
+		color = AgoniaData.MapData.terrains_by_id[terrain_id].terrain_color_default
+	paint_tile(Vector2i(_location.x, _location.y), color)
+
+
+func on_update_completed() -> void:
+	var new_texture = ImageTexture.create_from_image(display_image)
+	self.texture = new_texture
+
+
+func on_save_game_requested() -> void:
+	display_image.save_png("res://Flat Files/terrain_colors.png")
 
 
 func paint_tile(_position: Vector2i, _color: Color) -> void:
@@ -20,15 +50,9 @@ func paint_tile(_position: Vector2i, _color: Color) -> void:
 	display_image.fill_rect(tile, _color)
 
 
-func apply_image():
-	var new_texture = ImageTexture.create_from_image(display_image)
-	self.texture = new_texture
-
-
 func print_log_image_details(_coords, _mouse):
 	print(display_image)
 	print(display_image.get_size())
 	print(display_image.get_format())
 	print(self.texture.resource_name)
 	print(self.texture.resource_path)
-
