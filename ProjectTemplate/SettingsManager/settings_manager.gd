2c0d578eb943ec519320350553d252805e720538
David Barner <dibarner@gmail.com>
2025-12-09 20:48:44 -0800

    Update settings_manager.gd
    change config file to be encrypted

--- ProjectTemplate/SettingsManager/settings_manager.gd
+++ ProjectTemplate/SettingsManager/settings_manager.gd
@@ -2,16 +2,17 @@ extends Node
 
 signal settings_value_changed(key: String, new_value, old_value)
 
-const SETTINGS_FILE: String = "user://settings.cfg"
+const SETTINGS_FILE_PATH: String = "user://settings.cfg"
 const DEFAULT_SECTION: String = "user"
 
 var config: ConfigFile
-var settings: Dictionary = {} # String -> setting
+var settings: Dictionary[String, Variant] = {} # String -> setting
+var password = OS.get_unique_id()
 
 
 func _init() -> void:
 	config = ConfigFile.new()
-	config.load(SETTINGS_FILE)
+	config.load_encrypted_pass(SETTINGS_FILE_PATH, password)
 
 
 func _ready() -> void:
@@ -37,7 +38,7 @@ func get_value(key: String, section: String = DEFAULT_SECTION):
 func set_value(key: String, value, section: String = DEFAULT_SECTION) -> void:
 	var old_value = get_value(key, section)
 	config.set_value(section, key, value)
-	config.save(SETTINGS_FILE)
+	config.save_encrypted_pass(SETTINGS_FILE_PATH, password)
 	
 	settings_value_changed.emit(key, value, old_value)
 
